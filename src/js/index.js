d845ec2edb130f8df400e3c0eae2050571c8c86e
unknown <vmartxsm@gmail.com>
2020-06-06 13:36:27 +0300

    basic change

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// Yank theme color from localStorage and use it.
+document.documentElement.style.setProperty("--mainColor", localStorage.getItem("userThemeColor"));
+
+var colorInput = document.querySelector("#choose-theme-color");
+
+colorInput.addEventListener("change", function() {
+  
+  // Theme the site!
+  document.documentElement.style.setProperty("--mainColor", this.value);
+  
+  // Save the value for next time page is visited.
+  localStorage.setItem("userThemeColor", this.value);
+  
+});
